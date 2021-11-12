bb31211b0e6e028affcd7ab53750fb8241c4b75a
amie <amiec@marsreturnship.com>
2021-11-12 10:19:44 -0700

    new file

---
+++ boolean.js
@@ -0,0 +1,9 @@
+if (4) {
+  console.log('true');
+}
+else {
+  console.log(test);
+}
+
+// if anything that is not 0 or non-zero message, including a string,then it evaluates to true.
+// if 0, null, infinity, undefined, then false
